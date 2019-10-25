24191223cf3dea629d32503144d753fe82bb1bfa
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-10-24 18:52:52 -0700

    Método para devolver imágenes

--- routes/project.js
+++ routes/project.js
@@ -17,5 +17,6 @@ router.get('/projects', ProjectController.getProjects);
 router.put('/project-update/:id',ProjectController.updateProject);
 router.delete('/project-delete/:id',ProjectController.deleteProject);
 router.post('/upload-image/:id', multipartMiddleware,  ProjectController.uploadImage);
+router.get('/get-image/:image', ProjectController.getImageFile);
 
 module.exports = router;
